76ad927a3168d5182121546cc6fb93347aa07222
SOUFIANE AIT TALEB <93433227+SOUFIANE-AIT-TALEB@users.noreply.github.com>
2022-10-18 16:38:27 +0100

    P08 Atelier1 Begin

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
-import '../styles/Banner.css'
+import Banner from './Banner'
+import logo from '../assets/logo.png'
+// import Cart from './Cart'
+import ShoppingList from './ShoppingList'
 
-function Banner({ children }) {
-	return <div className='lmj-banner'>{children}</div>
+function App() {
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			{/* <Cart /> */}
+			<ShoppingList />
+		</div>
+	)
 }
 
-export default Banner
+export default App
